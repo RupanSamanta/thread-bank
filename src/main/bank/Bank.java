5dd370fdb1b0ad8f73fd9b481c81b2cf38507f37
Rupan Samanta <rvsjan32@gmail.com>
2026-08-09 00:34:55 +0530

    Refactor Bank class to implement deposit, withdraw, transfer, and getBankBalance methods

--- src/main/bank/Bank.java
+++ src/main/bank/Bank.java
@@ -1,6 +1,8 @@
 package main.bank;
 import java.util.HashMap;
 import java.util.Map;
+
+import main.exception.InsufficientBalanceException;
 import main.model.Account;
 
 public class Bank {
@@ -20,5 +22,28 @@ public class Bank {
 
     public Account findAccount(int accountNumber) {
         return accounts.get(accountNumber);
+    }
+
+    public void deposit(int accountNumber, double amount) {
+        Account account = this.findAccount(accountNumber);
+        account.deposit(amount);
+    }
+
+    public void withdraw(int accountNumber, double amount) throws InsufficientBalanceException {
+        Account account = this.findAccount(accountNumber);
+        account.withdraw(amount);
+    }
+
+    public void transfer(int senderId, int receiverId, double amount) throws InsufficientBalanceException {
+        Account sender = this.findAccount(senderId);
+        Account receiver = this.findAccount(receiverId);
+
+        sender.withdraw(amount);
+        receiver.deposit(amount);
+    }
+
+    public double getBankBalance(int accountNumber) {
+        Account account = this.findAccount(accountNumber);
+        return account.getBalance();
     }
 }
